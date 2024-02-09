dfe8e8debbc4ea484ce1316a28cc565586e55be2
rodolforsf <rodolfofaria46@gmail.com>
2024-02-08 22:11:20 -0300

    Criando CardButton

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
 
 import './SearchBar.css';
 
 function SearchBar() {
+
+  const [searchValue, setSearchValue] = useState('');
+
   return (
     <form className="search-bar">
       <input 
@@ -11,7 +14,7 @@ function SearchBar() {
         placeholder= "Buscar produtos"
         className= "search__input"
       />
-
+      { searchValue }
       <button type="submit" className="search__button">
         <BsSearch />
       </button>
