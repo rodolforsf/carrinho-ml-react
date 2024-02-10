d63ae5f72b81ad6027f372d83c7520e38e4e8568
Rodolfo Faria <106396728+rodolforsf@users.noreply.github.com>
2024-02-10 11:20:07 -0300

    Criando AppContext e Provider

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,22 +1,35 @@
 import React, { useState } from 'react';
-import { BsSearch } from 'react-icons/bs';
+import { IoSearchSharp } from 'react-icons/io5';
 
 import './SearchBar.css';
+import fetchProducts from '../../api/fetchProducts';
 
 function SearchBar() {
 
   const [searchValue, setSearchValue] = useState('');
-  console.log(setSearchValue);
+  
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    
+    const products = await fetchProducts(searchValue);
+    
+    console.log(products);
+    setSearchValue('');
+  };
+
   return (
-    <form className="search-bar">
+    <form className="search-bar" onSubmit={handleSearch}>
       <input 
         type="search" 
+        value={searchValue}
         placeholder= "Buscar produtos"
         className= "search__input"
+        onChange={ ({ target }) => setSearchValue(target.value)}
+        required
       />
-      { searchValue }
+      
       <button type="submit" className="search__button">
-        <BsSearch />
+        <IoSearchSharp />
       </button>
     </form>
   );
---
+++ src/context/Provider.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppContext from './AppContext';
+
+function Provider({}) {
+  return ( 
+    <AppContext.Provider>
+      
+    </AppContext.Provider>
+   );
+}
+
+export default Provider;
