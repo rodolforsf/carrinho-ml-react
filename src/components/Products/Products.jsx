b962ae2adab1a386febe5fa84ef7b628255cef05
rodolforsf <rodolfofaria46@gmail.com>
2024-02-10 07:09:52 -0300

    atualizando

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './Products.css';
+import fetchProducts from '../../api/fetchProdutcts';
 
 function Products() {
 
   const [products, setProducts] = useState([]);
+
+  console.log(products);
+  console.log(setProducts);
+  useEffect(() => {
+    fetchProducts('iphone');
+  }, []);
 
   return(
     <section className="products container">
