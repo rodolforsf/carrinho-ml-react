61887f0b28b36fe22dd26f1916a4401c3c325461
rodolforsf <rodolfofaria46@gmail.com>
2024-02-10 06:58:00 -0300

    Criando arquivo Products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './Products.css';
 
 function Products() {
+
+  const [products, setProducts] = useState([]);
+
   return(
-    <section className="products container">teste</section>
+    <section className="products container">
+      teste
+    </section>
   );
 }
 
